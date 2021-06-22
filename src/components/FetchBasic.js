c5d3343a35306cb1eca714d664a1138ee930656b
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-06-22 16:04:16 -0700

    fetch basic and sandbox embed

--- src/components/FetchBasic.js
+++ src/components/FetchBasic.js
@@ -25,14 +25,30 @@ const FetchBasic = () => {
       <h4> A basic fetch</h4>
       <section>
         <p>
-          Here I am using the{' '}
+          Below I am using the{' '}
           <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API">
             fetch-API
           </a>{' '}
           to retrieve a single user from the{' '}
-          <a href="https://randomuser.me/">Random User Generator</a>.
+          <a href="https://randomuser.me/">Random User Generator</a> API.
         </p>
         <p>
+          Below is a code snippet showing the actual fetch. We want to fetch
+          after all other resources have been loaded onto your page or
+          component. There are a couple of ways of achieving this in React, for
+          example by using the useEffect hook, or wrapping the request in{' '}
+          <a href="https://reactjs.org/docs/react-component.html">
+            componentDidMount()
+          </a>
+          .
+        </p>
+        <p>
+          I am using the useEffect hook with an open dependency array, which
+          will have the effect of making the fetch request once, when the
+          component has loaded.
+        </p>
+        <p>
+          <p className="sub-header">Code</p>
           <img
             className="code_img"
             src={fetchBasic}
@@ -40,6 +56,7 @@ const FetchBasic = () => {
             width="100%"
           />
         </p>
+        <p className="sub-header">Result</p>
       </section>
       <section>
         <article>
@@ -53,6 +70,22 @@ const FetchBasic = () => {
           </div>
           <p>Lives in {`${data.location.city}, ${data.location.country}`}</p>
         </article>
+      </section>
+      <section>
+        <p class="sub-header">Sandbox</p>
+        <iframe
+          src="https://codesandbox.io/embed/runtime-bash-ty05y?fontsize=14&hidenavigation=1&theme=dark"
+          style={{
+            width: '100%',
+            height: '500px',
+            border: '0',
+            borderRadius: '4px',
+            overflow: 'hidden',
+          }}
+          title="runtime-bash-ty05y"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
       </section>
     </div>
   );
